4257f032faf3e6b8213af13157a619acb1fe419c
송문수 <private1023@naver.com>
2021-08-26 14:12:21 +0000

    Array1 연습 코드 추가(210826)

---
+++ src/1. 배열과 문자열/연습 코드/Array1.java
@@ -0,0 +1,12 @@
+public class Array1{
+    public static boolean StringCheck(String array){
+        boolean[] ChrArray = new boolean[128];
+
+        for(int i = 0; i<array.length(); i++){
+            char chr = array.charAt(i);
+            if(ChrArray[(int)chr] == true) return false;
+            ChrArray[(int)chr] = true;
+        }
+        return true;
+    }
+}
