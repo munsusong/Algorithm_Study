c8cac12ed7c92becee01662c16975ee021fff7e0
송문수 <private1023@naver.com>
2021-09-02 14:36:09 +0000

    Array6 연습코드 완성(210902)

---
+++ src/1. 배열과 문자열/연습 코드/Array6.java
@@ -0,0 +1,27 @@
+public class Array6{
+    public String returnString(String str) {
+        if (str.length() < compressString(str).length()) {
+            return str;
+        } else {
+            return compressString(str);
+        }
+    }
+    public String compressString(String str) {
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i ++) {
+            if (i != 0 && str.charAt(i) != str.charAt(i - 1)) {
+                sb.append(str.charAt(i - 1));
+                sb.append(count);
+                count = 1;
+                continue;
+            }
+            if (i == str.length() - 1) {
+                sb.append(str.charAt(i));
+                sb.append(count + 1);
+            }
+            count ++;
+        }
+        return sb.toString();
+    }
+}
