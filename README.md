45fcff9958fd9bf833cb5b8a2f4fc1e7f3257756
송문수 <private1023@naver.com>
2021-10-02 14:58:25 +0000

    list1 연습코드 완성 및 read.md 수정 (211002)

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 ***
 ###  자료구조 (Data Structure)
 1. [배열과 문자열 (Array And String)](https://github.com/munsusong/Data_Structure_And_Algorithm_Study/tree/master/src/1.%20%EB%B0%B0%EC%97%B4%EA%B3%BC%20%EB%AC%B8%EC%9E%90%EC%97%B4)
-
+2. [연결리스트 (Linked List)](https://github.com/munsusong/Data_Structure_And_Algorithm_Study/tree/master/src/2.%20%EC%97%B0%EA%B2%B0%EB%A6%AC%EC%8A%A4%ED%8A%B8%20%ED%95%B4%EB%B2%95)
 
 ***
 ### 계획
--- src/2. 연결리스트 해법/연습 코드/List1.java
+++ src/2. 연결리스트 해법/연습 코드/List1.java
@@ -1,20 +1,22 @@
 public class List1{
     public static void main(String[] args) {
-        LinkedList list = new LinkedList<Integer>(Arrays.asList(1,2,3,3,3,4));    
+        LinkedList list = new LinkedList<Integer>(Arrays.asList(1, 2, 3, 3, 3, 4));
         deleteDups(list);
-    }   
-    public static void deleteDups(LinkedList list){
+    }
+    public static void deleteDups(LinkedList list) {
         HashSet hs = new HashSet();
-        for(int i = 0; i<list.size(); i++){
-            if(hs.contains(list.get(i))){
+        for (int i = 0; i < list.size(); i ++) {
+            if (hs.contains(list.get(i))) {
                 list.remove(i);
-                i--;
+                i --;
+                System.out.println("delete");
+            } else {
+                hs.add(list.get(i));
             }
-            hs.add(list.get(i));
-            System.out.println(list.size());
+            System.out.println(list.size() + "  " + i);
         }
-        for(int i = 0; i<list.size(); i++){
-             System.out.println(list.get(i));
+        for (int i = 0; i < list.size(); i ++) {
+            System.out.println(list.get(i));
         }
-}
+    }
 }
